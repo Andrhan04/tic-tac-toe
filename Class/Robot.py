037606980815139aba4dcb7bb732ca4286d74625
Andrhan04 <andrham2004@yandex.ru>
2025-05-12 12:49:22 +0800

    add slep in bot

--- Class/Robot.py
+++ Class/Robot.py
@@ -1,7 +1,7 @@
 from telebot import types
 from configurate.Logs import write_log, write_log_exeption, write_exeption
 import Class.Query as q
-
+import time as t
 
 def Best_Step(message : types.Message, id : int):
     if(id == -1):
@@ -16,6 +16,7 @@ def Best_Step(message : types.Message, id : int):
 # Dodo
 def Best_Step_Dodo():
     try:
+        t.sleep(1)
         game_id : int = q.GetGame(-1)
         x,y = q.GetBestStep_Dodo(game_id,-1)
         q.Step(-1, x, y)
@@ -29,6 +30,7 @@ def Best_Step_Dodo():
 
 def Best_Step_IT():
     try:
+        t.sleep(1)
         game_id : int = q.GetGame(-2)
         x,y = q.GetBestStep_IT(game_id,-2)
         q.Step(-2, x, y)
